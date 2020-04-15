2ec1710bb36b3701450ae899a0879c0bf02ddbdc
Jan Sladký <sladky.honza@gmail.com>
2020-04-15 15:50:42 +0200

    obrázky a media.php

--- media.php
+++ media.php
@@ -8,54 +8,94 @@
     <div class="column">
     <div class="gallery">
     <a href="assets/image/image_galerie/tisk/tisk000.jpg" data-lightbox="mygallery" data-title="Žatecký a lounský deník, 17. října 2018" title="Žatecký a lounský deník, 17. října 2018">
-    <img src="assets/image/image_galerie/tisk/tisk000_small.jpg" alt="Novinový článek" /> </a> 
-        <a href="assets/image/image_galerie/tisk/tisk00.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 28. února 2018" title="Žatecké noviny, 28. února 2018"> 
-        <img src="assets/image/image_galerie/tisk/tisk00_small.jpg" alt="Novinový článek" /> </a> 
-        <a href="assets/image/image_galerie/tisk/tisk01.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 24. dubna 2018" title="Žatecké noviny, 24. dubna 2018"> 
-        <img src="assets/image/image_galerie/tisk/tisk01_small.jpg" alt="Novinový článek" /> </a> 
-        <a href="assets/image/image_galerie/tisk/tisk02.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 24. dubna 2018" title="Žatecké noviny, 24. dubna 2018"> 
-        <img src="assets/image/image_galerie/tisk/tisk02_small.jpg" alt="Novinový článek" /> </a> 
-        <a href="assets/image/image_galerie/tisk/tisk03.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 16. května 2018" title="Žatecké noviny, 16. května 2018"> 
-        <img src="assets/image/image_galerie/tisk/tisk03_small.jpg" alt="Novinový článek" /> </a> 
-        <a href="assets/image/image_galerie/tisk/tisk04.jpg" data-lightbox="mygallery" data-title="Žatecký a lounský deník, 23. května 2018" title="Žatecký a lounský deník, 23. května 2018"> 
-        <img src="assets/image/image_galerie/tisk/tisk04_small.jpg" alt="Novinový článek" /> </a> 
-        <a href="assets/image/image_galerie/tisk/tisk05.jpg" data-lightbox="mygallery" data-title="Žatecký a lounský deník, 23. května 2018" title="Žatecký a lounský deník, 23. května 2018"> 
-        <img src="assets/image/image_galerie/tisk/tisk05_small.jpg" alt="Novinový článek" /> </a> 
-        <a href="assets/image/image_galerie/tisk/tisk07.jpg" data-lightbox="mygallery" data-title="Žatecký zpravodaj, červen 2018" title="Žatecký zpravodaj, červen 2018"> 
-        <img src="assets/image/image_galerie/tisk/tisk07_small.jpg" alt="Novinový článek" /> </a> 
-        <a href="assets/image/image_galerie/tisk/tisk08.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 13. června 2018" title="Žatecké noviny, 13. června 2018"> 
-        <img src="assets/image/image_galerie/tisk/tisk08_small.jpg" alt="Novinový článek" /> </a> 
-        <a href="assets/image/image_galerie/tisk/tisk09.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 20. června 2018" title="Žatecké noviny, 20. června 2018"> 
-        <img src="assets/image/image_galerie/tisk/tisk09_small.jpg" alt="Novinový článek" /> </a> 
-        <a href="assets/image/image_galerie/tisk/tisk10.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 20. června 2018" title="Žatecké noviny, 20. června 2018"> 
-        <img src="assets/image/image_galerie/tisk/tisk10_small.jpg" alt="Novinový článek" /> </a> 
-        <a href="assets/image/image_galerie/tisk/tisk11.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 20. června 2018" title="Žatecké noviny, 20. června 2018"> 
-        <img src="assets/image/image_galerie/tisk/tisk11_small.jpg" alt="Novinový článek" /> </a> 
-        <a href="assets/image/image_galerie/tisk/tisk12.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 4. července 2018" title="Žatecké noviny, 4. července 2018"> 
-        <img src="assets/image/image_galerie/tisk/tisk12_small.jpg" alt="Novinový článek" /> </a> 
-        <a href="assets/image/image_galerie/tisk/tisk13.jpg" data-lightbox="mygallery" data-title="Žatecký a lounský deník, 9. července 2018" title="Žatecký a lounský deník, 9. července 2018"> 
-        <img src="assets/image/image_galerie/tisk/tisk13_small.jpg" alt="Novinový článek" /></a> 
-        <a href="assets/image/image_galerie/tisk/tisk15.jpg" data-lightbox="mygallery" data-title="Žatecký zpravodaj, srpen 2018" title="Žatecký zpravodaj, srpen 2018"> 
-        <img src="assets/image/image_galerie/tisk/tisk15_small.jpg" alt="Novinový článek" /> </a> 
-        <a href="assets/image/image_galerie/tisk/tisk16.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 1. srpna 2018" title="Žatecké noviny, 1. srpna 2018"> 
-        <img src="assets/image/image_galerie/tisk/tisk16_small.jpg" alt="Novinový článek" /> </a> 
-        <a href="assets/image/image_galerie/tisk/tisk17.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 19. září 2018" title="Žatecké noviny, 19. září 2018"> 
-        <img src="assets/image/image_galerie/tisk/tisk17_small.jpg" alt="Novinový článek" /> </a> 
-        <a href="assets/image/image_galerie/tisk/tisk18.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 19. září 2018" title="Žatecké noviny, 19. září 2018"> 
-        <img src="assets/image/image_galerie/tisk/tisk18_small.jpg" alt="Novinový článek" /> </a> 
-        <a href="assets/image/image_galerie/tisk/tisk19.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 26 září 2018" title="Žatecké noviny, 26 září 2018"> 
-        <img src="assets/image/image_galerie/tisk/tisk19_small.jpg" alt="Novinový článek" /> </a> 
-        <a href="assets/image/image_galerie/tisk/tisk20.jpg" data-lightbox="mygallery" data-title="Žatecký zpravodaj, říjen 2018" title="Žatecký zpravodaj, říjen 2018"> 
-        <img src="assets/image/image_galerie/tisk/tisk20_small.jpg" alt="Novinový článek" /> </a> 
-        <a href="assets/image/image_galerie/tisk/tisk21.jpg" data-lightbox="mygallery" data-title="Žatecký zpravodaj, říjen 2018" title="Žatecký zpravodaj, říjen 2018"> 
-        <img src="assets/image/image_galerie/tisk/tisk21_small.jpg" alt="Novinový článek" /> </a> 
-        <a href="assets/image/image_galerie/tisk/tisk22.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 10. října 2018" title="Žatecké noviny, 10. října 2018"> 
+    <img src="assets/image/image_galerie/tisk/tisk000_small.jpg" alt="Novinový článek" /> </a>
+        <a href="assets/image/image_galerie/tisk/tisk00.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 28. února 2018" title="Žatecké noviny, 28. února 2018">
+        <img src="assets/image/image_galerie/tisk/tisk00_small.jpg" alt="Novinový článek" /> </a>
+        <a href="assets/image/image_galerie/tisk/tisk01.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 24. dubna 2018" title="Žatecké noviny, 24. dubna 2018">
+        <img src="assets/image/image_galerie/tisk/tisk01_small.jpg" alt="Novinový článek" /> </a>
+        <a href="assets/image/image_galerie/tisk/tisk02.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 24. dubna 2018" title="Žatecké noviny, 24. dubna 2018">
+        <img src="assets/image/image_galerie/tisk/tisk02_small.jpg" alt="Novinový článek" /> </a>
+        <a href="assets/image/image_galerie/tisk/tisk03.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 16. května 2018" title="Žatecké noviny, 16. května 2018">
+        <img src="assets/image/image_galerie/tisk/tisk03_small.jpg" alt="Novinový článek" /> </a>
+        <a href="assets/image/image_galerie/tisk/tisk04.jpg" data-lightbox="mygallery" data-title="Žatecký a lounský deník, 23. května 2018" title="Žatecký a lounský deník, 23. května 2018">
+        <img src="assets/image/image_galerie/tisk/tisk04_small.jpg" alt="Novinový článek" /> </a>
+        <a href="assets/image/image_galerie/tisk/tisk05.jpg" data-lightbox="mygallery" data-title="Žatecký a lounský deník, 23. května 2018" title="Žatecký a lounský deník, 23. května 2018">
+        <img src="assets/image/image_galerie/tisk/tisk05_small.jpg" alt="Novinový článek" /> </a>
+        <a href="assets/image/image_galerie/tisk/tisk07.jpg" data-lightbox="mygallery" data-title="Žatecký zpravodaj, červen 2018" title="Žatecký zpravodaj, červen 2018">
+        <img src="assets/image/image_galerie/tisk/tisk07_small.jpg" alt="Novinový článek" /> </a>
+        <a href="assets/image/image_galerie/tisk/tisk08.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 13. června 2018" title="Žatecké noviny, 13. června 2018">
+        <img src="assets/image/image_galerie/tisk/tisk08_small.jpg" alt="Novinový článek" /> </a>
+        <a href="assets/image/image_galerie/tisk/tisk09.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 20. června 2018" title="Žatecké noviny, 20. června 2018">
+        <img src="assets/image/image_galerie/tisk/tisk09_small.jpg" alt="Novinový článek" /> </a>
+        <a href="assets/image/image_galerie/tisk/tisk10.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 20. června 2018" title="Žatecké noviny, 20. června 2018">
+        <img src="assets/image/image_galerie/tisk/tisk10_small.jpg" alt="Novinový článek" /> </a>
+        <a href="assets/image/image_galerie/tisk/tisk11.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 20. června 2018" title="Žatecké noviny, 20. června 2018">
+        <img src="assets/image/image_galerie/tisk/tisk11_small.jpg" alt="Novinový článek" /> </a>
+        <a href="assets/image/image_galerie/tisk/tisk12.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 4. července 2018" title="Žatecké noviny, 4. července 2018">
+        <img src="assets/image/image_galerie/tisk/tisk12_small.jpg" alt="Novinový článek" /> </a>
+        <a href="assets/image/image_galerie/tisk/tisk13.jpg" data-lightbox="mygallery" data-title="Žatecký a lounský deník, 9. července 2018" title="Žatecký a lounský deník, 9. července 2018">
+        <img src="assets/image/image_galerie/tisk/tisk13_small.jpg" alt="Novinový článek" /></a>
+        <a href="assets/image/image_galerie/tisk/tisk15.jpg" data-lightbox="mygallery" data-title="Žatecký zpravodaj, srpen 2018" title="Žatecký zpravodaj, srpen 2018">
+        <img src="assets/image/image_galerie/tisk/tisk15_small.jpg" alt="Novinový článek" /> </a>
+        <a href="assets/image/image_galerie/tisk/tisk16.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 1. srpna 2018" title="Žatecké noviny, 1. srpna 2018">
+        <img src="assets/image/image_galerie/tisk/tisk16_small.jpg" alt="Novinový článek" /> </a>
+        <a href="assets/image/image_galerie/tisk/tisk17.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 19. září 2018" title="Žatecké noviny, 19. září 2018">
+        <img src="assets/image/image_galerie/tisk/tisk17_small.jpg" alt="Novinový článek" /> </a>
+        <a href="assets/image/image_galerie/tisk/tisk18.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 19. září 2018" title="Žatecké noviny, 19. září 2018">
+        <img src="assets/image/image_galerie/tisk/tisk18_small.jpg" alt="Novinový článek" /> </a>
+        <a href="assets/image/image_galerie/tisk/tisk19.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 26 září 2018" title="Žatecké noviny, 26 září 2018">
+        <img src="assets/image/image_galerie/tisk/tisk19_small.jpg" alt="Novinový článek" /> </a>
+        <a href="assets/image/image_galerie/tisk/tisk20.jpg" data-lightbox="mygallery" data-title="Žatecký zpravodaj, říjen 2018" title="Žatecký zpravodaj, říjen 2018">
+        <img src="assets/image/image_galerie/tisk/tisk20_small.jpg" alt="Novinový článek" /> </a>
+        <a href="assets/image/image_galerie/tisk/tisk21.jpg" data-lightbox="mygallery" data-title="Žatecký zpravodaj, říjen 2018" title="Žatecký zpravodaj, říjen 2018">
+        <img src="assets/image/image_galerie/tisk/tisk21_small.jpg" alt="Novinový článek" /> </a>
+        <a href="assets/image/image_galerie/tisk/tisk22.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 10. října 2018" title="Žatecké noviny, 10. října 2018">
         <img src="assets/image/image_galerie/tisk/tisk22_small.jpg" alt="Novinový článek" /> </a>
-        <a href="assets/image/image_galerie/tisk/tisk23.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 17. dubna 2019" title="Žatecké noviny, 17. dubna 2019"> 
+        <a href="assets/image/image_galerie/tisk/tisk23.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 17. dubna 2019" title="Žatecké noviny, 17. dubna 2019">
         <img src="assets/image/image_galerie/tisk/tisk23_small.jpg" alt="Novinový článek" /> </a>
-        <a href="assets/image/image_galerie/tisk/tisk24.JPG" data-lightbox="mygallery" data-title="Žatecký zpravodaj, 2. května 2019" title="Žatecký zpravodaj, 2. května 2019"> 
+
+        <a href="assets/image/image_galerie/tisk/tisk24.JPG" data-lightbox="mygallery" data-title="Žatecký zpravodaj, 2. května 2019" title="Žatecký zpravodaj, 2. května 2019">
         <img src="assets/image/image_galerie/tisk/tisk24_small.JPG" alt="Novinový článek" /> </a>
-        
+
+        <a href="assets/image/image_galerie/tisk/tisk25.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 19. června 2019" title="Žatecký zpravodaj, 2. května 2019">
+        <img src="assets/image/image_galerie/tisk/tisk25_small.jpg" alt="Novinový článek" /> </a>
+
+        <a href="assets/image/image_galerie/tisk/tisk26.jpg" data-lightbox="mygallery" data-title=" Žatecký deník, 24. června 2019" title="Žatecký zpravodaj, 2. května 2019">
+        <img src="assets/image/image_galerie/tisk/tisk26_small.jpg" alt="Novinový článek" /> </a>
+
+        <a href="assets/image/image_galerie/tisk/tisk27.jpg" data-lightbox="mygallery" data-title=" Žatecké noviny, 26. června 2019" title="Žatecký zpravodaj, 2. května 2019">
+        <img src="assets/image/image_galerie/tisk/tisk27_small.JPG" alt="Novinový článek" /> </a>
+
+        <a href="assets/image/image_galerie/tisk/tisk28.jpg" data-lightbox="mygallery" data-title="5 plus 2, 9. srpna 2019" title="Žatecký zpravodaj, 2. května 2019">
+        <img src="assets/image/image_galerie/tisk/tisk28_small.jpg" alt="Novinový článek" /> </a>
+
+        <a href="assets/image/image_galerie/tisk/tisk29.jpg" data-lightbox="mygallery" data-title=" Žatecké noviny, 28. srpna 2019" title="Žatecký zpravodaj, 2. května 2019">
+        <img src="assets/image/image_galerie/tisk/tisk29_small.jpg" alt="Novinový článek" /> </a>
+
+        <a href="assets/image/image_galerie/tisk/tisk30.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 2. října 2019" title="Žatecký zpravodaj, 2. května 2019">
+        <img src="assets/image/image_galerie/tisk/tisk30_small.jpg" alt="Novinový článek" /> </a>
+
+        <a href="assets/image/image_galerie/tisk/tisk31.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 27. listopadu 2019" title="Žatecký zpravodaj, 2. května 2019">
+        <img src="assets/image/image_galerie/tisk/tisk31_small.jpg" alt="Novinový článek" /> </a>
+
+        <a href="assets/image/image_galerie/tisk/tisk32.jpg" data-lightbox="mygallery" data-title=" Žatecký zpravodaj, prosinec 2019" title="Žatecký zpravodaj, 2. května 2019">
+        <img src="assets/image/image_galerie/tisk/tisk32_small.jpg" alt="Novinový článek" /> </a>
+
+        <a href="assets/image/image_galerie/tisk/tisk33.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 27. prosince 2019" title="Žatecký zpravodaj, 2. května 2019">
+        <img src="assets/image/image_galerie/tisk/tisk33_small.jpg" alt="Novinový článek" /> </a>
+
+        <a href="assets/image/image_galerie/tisk/tisk34.jpg" data-lightbox="mygallery" data-title=" Žatecké noviny, 15. ledna 2020" title="Žatecký zpravodaj, 2. května 2019">
+        <img src="assets/image/image_galerie/tisk/tisk34_small.jpg" alt="Novinový článek" /> </a>
+
+        <a href="assets/image/image_galerie/tisk/tisk35.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 22. ledna 2020" title="Žatecký zpravodaj, 2. května 2019">
+        <img src="assets/image/image_galerie/tisk/tisk35_small.jpg" alt="Novinový článek" /> </a>
+
+        <a href="assets/image/image_galerie/tisk/tisk36.jpg" data-lightbox="mygallery" data-title=" Žatecké noviny, 5. února 2020" title="Žatecký zpravodaj, 2. května 2019">
+        <img src="assets/image/image_galerie/tisk/tisk36_small.jpg" alt="Novinový článek" /> </a>
+
+        <a href="assets/image/image_galerie/tisk/tisk37.jpg" data-lightbox="mygallery" data-title="Žatecké noviny, 19. února 2020" title="Žatecký zpravodaj, 2. května 2019">
+        <img src="assets/image/image_galerie/tisk/tisk37_small.jpg" alt="Novinový článek" /> </a>
+
     </div>
     </div>
 
