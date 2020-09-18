f8ff1e14e1ac16e17e5c8e79176ab8f03207b040
JanSladky <sladky.honza@gmail.com>
2020-09-18 11:09:52 +0200

    aktualizace připravovaných

--- pripravovane.php
+++ pripravovane.php
@@ -16,18 +16,6 @@
       </ul>
       Tak v sobotu ahoj! <br> <a href="https://www.instagram.com/explore/tags/zachranmezlatychmel/?hl=cs" class="link" target="_blank">#zachranmezlatychmel</a> 
       </p>
-      <div class="hr"></div>
-      
-
-      <p class="justify">
-        Ahoj Adálisti! <br>
-        Svět se pomalu vrací do normálu, tedy alespoň ten náš, malý kulturní. Co letos bude?
-        Protože se jaro chýlí ke konci, II. Jarní cyklovyjížďku přesouváme na březen 2021. Do té doby můžete mapovat trasu ve <a href="https://prezi.com/view/crGdMQayl6kn4YcOom6N/" class="link" target="_blank">virtuální projížďce</a> O co ale nepřijdete je greenswap Pošli dál...pokojovky.
-        <div class="poster">
-          <a href="https://www.facebook.com/events/844110372781686/"><img src="assets/image/pripravovane/swap-01.jpg" target="_blank" alt="swap_plakát"></a>
-        </div>
-      </p>
-
     </div>
   </div>
 </main>
