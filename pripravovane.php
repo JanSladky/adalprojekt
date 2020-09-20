3465c04aa6c8df2e8602c9ec99342196cbed230d
JanSladky <sladky.honza@gmail.com>
2020-09-20 15:36:52 +0200

    úpravaúprava obrázku na proklik na akci u připravovaných

--- pripravovane.php
+++ pripravovane.php
@@ -8,7 +8,7 @@
         Celorepublikový festival Den architektury slaví 10 let a jeho pořadatel Kruh celých dvacet. No, a my se účastníme potřetí. Tentokrát s velmi zajímavým tématem kterým jsou: PRÁZDNÉ DOMY! Společně si projdeme 4 objekty v Žatci. A kdy a kde začneme? Sraz se koná v sobotu, 3. října ve 14 hodin na Nerudově náměstí. Těšíme se!  
       </p>
       <div class="poster">
-        <img src="assets/image/pripravovane/DA_2020_A3.png" alt="plakát na DA2020">
+        <a href="https://www.facebook.com/events/985954675196721/?acontext=%7B%22event_action_history%22%3A[%7B%22mechanism%22%3A%22search_results%22%2C%22surface%22%3A%22search%22%7D]%7D" target="_blank"><img src="assets/image/pripravovane/DA_2020_A3.png" alt="plakát na DA2020"></a> 
       </div>
     </div>
   </div>
